0f9aac1ba27c401fb8d863b62695c58bd00a61f8
Mondeep Kalita <epicdeep09@gmail.com>
2023-04-05 23:00:15 +0530

    res.write() instead of res.send()

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Hello from the Express!!")
+    // res.send("Hello from the Express!!")
+    res.write("<h1>Hello from the Express!!</h1>")
+    res.write("<h2>Hello from the Express!!</h2>")
+    res.send()
+})
+
+app.get("/about", (req, res) => {
+    res.status(200).send("Hello from the About Page!!")
+    // res.status(200).send("Hello from the About Page!!")
 })
 
 app.listen(9000, () => {
